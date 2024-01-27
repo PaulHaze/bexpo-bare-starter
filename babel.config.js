994d6441fa4c8839bb3298f1f4d9b90325bcac5d
PaulHaze <dev@artemist.com.au>
2024-01-28 10:38:06 +1100

    add react native reanimated

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // plugins: [
-    //   [
-    //     'module-resolver',
-    //     {
-    //       root: ['.'],
-    //       alias: {
-    //         '@/styles': './src/styles',
-    //       },
-    //     },
-    //   ],
-    // ],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
